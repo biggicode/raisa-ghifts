644c7852650234823cf9fbad366d440b2a689ec4
Lari Oncescu <larioncescu@yahoo.com>
2021-03-30 20:24:17 +0300

    Adding files for top menu

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import { Button } from '../component/button'
+import { TopMenu } from '../component/top-menu'
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <TopMenu />
+    </div>
   )
 }
